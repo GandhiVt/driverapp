23cc86e5379033834e48c78103d82f2625958c18
GandhiVt <gandhi.vera@gmail.com>
2017-05-29 19:11:55 -0500

    act
    alizado

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://patraer.com/driver/api",					
+	'DialogDefaultTitle':"Repartidores",	
+	'PushProjectID':"409670212325",	
+	'APIHasKey':"N102SyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
+};
