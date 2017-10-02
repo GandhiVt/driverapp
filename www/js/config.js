42be2525e6d90ac8d9b1a984684423f9c5bef406
GandhiVt <gandhi.vera@gmail.com>
2017-10-01 19:45:59 -0500

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={	
-	'ApiUrl':"http://patraer.com/driver/api",					
+	'ApiUrl':"http://www.patraer.com/merchantapp/api",					
 	'DialogDefaultTitle':"Repartidores",	
 	'PushProjectID':"409670212325",	
-	'APIHasKey':"N102SyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
+	'APIHasKey':"AIzaSyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
 };
